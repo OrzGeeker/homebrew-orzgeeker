4c40b1e9ad975127ed775fc0979a1a4f11397921
wangzhizhou <824219521@qq.com>
2022-03-21 00:24:08 +0800

    add OrzTinyPNG-0.0.1

---
+++ OrzTinyPNG.rb
@@ -0,0 +1,36 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Orztinypng < Formula
+  desc "A command line tools for image compress of JPEG and PNG Using TinyPNG API"
+  homepage "https://github.com/OrzGeeker/OrzTinyPNG"
+  url "https://github.com/OrzGeeker/OrzTinyPNG/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "29eaa340019a8aabfc25eead187ab8da24183dd026e05516cc031c6dca2dec61"
+  license "Unlicense"
+
+  depends_on xcode: "13.3"
+  uses_from_macos "swift"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "swift", "build", "--disable-sandbox", "-c"， "release", "-Xswiftc", \
+    "--cross-module-optimization"
+    mv ".build/release/tiny", "tiny"
+    bin.install "tiny"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test OrzTinyPNG`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
