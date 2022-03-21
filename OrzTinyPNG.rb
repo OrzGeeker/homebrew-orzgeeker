ebee8138be54fa88de3826237f143da9e12c037c
wangzhizhou <wangzhizhou@bytedance.com>
2022-03-21 14:35:56 +0800

    rename with lowercase

--- OrzTinyPNG.rb
+++ OrzTinyPNG.rb
@@ -12,9 +12,6 @@ class Orztinypng < Formula
   uses_from_macos "swift"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "swift", "build", "--disable-sandbox", "--configuration", "release", \
     "-Xswiftc", "--cross-module-optimization"
     mv ".build/release/tiny", "tiny"
@@ -22,15 +19,6 @@ class Orztinypng < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test OrzTinyPNG`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
