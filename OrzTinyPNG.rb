d89bd238d8b703b212225f9f1a2a944c9b776e02
wangzhizhou <wangzhizhou@bytedance.com>
2022-03-21 14:51:14 +0800

    refine

--- OrzTinyPNG.rb
+++ OrzTinyPNG.rb
@@ -1,8 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+# typed: false
+# frozen_string_literal: true
+
+# https://docs.brew.sh/Formula-Cookbook
+# https://rubydoc.brew.sh/Formula
 class Orztinypng < Formula
-  desc "A command line tools for image compress of JPEG and PNG Using TinyPNG API"
+  desc "This is a command-line tool for image compress of JPEG and PNG Using TinyPNG API"
   homepage "https://github.com/OrzGeeker/OrzTinyPNG"
   url "https://github.com/OrzGeeker/OrzTinyPNG/archive/refs/tags/0.0.3.tar.gz"
   sha256 "f1466a4368375a6c4b94c2d96616ee90221f2d176ef27113ad97ea4273d9db93"
@@ -12,8 +14,8 @@ class Orztinypng < Formula
   uses_from_macos "swift"
 
   def install
-    system "swift", "build", "--disable-sandbox", "--configuration", "release", \
-    "-Xswiftc", "--cross-module-optimization"
+    system "swift", "build", "--disable-sandbox", "--configuration", "release",
+           "-Xswiftc", "--cross-module-optimization"
     mv ".build/release/tiny", "tiny"
     bin.install "tiny"
   end
