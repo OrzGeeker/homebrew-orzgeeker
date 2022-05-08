740606f3e80780fa30160f600813b70cfcb83198
wangzhizhou <824219521@qq.com>
2022-05-08 18:09:32 +0800

    update orzswiftmc to 0.0.2

--- orzswiftmc.rb
+++ orzswiftmc.rb
@@ -7,8 +7,8 @@
 class Orzswiftmc < Formula
   desc "This is a command-line tool for deploy minecraft server or launch client"
   homepage ""
-  url "https://github.com/OrzGeeker/OrzSwiftMC/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "478fc4e05865cbf28f5574113b34ef0b60c5cb0f51384a4daffc9b720cb36845"
+  url "https://github.com/OrzGeeker/OrzSwiftMC/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "1142cc7169d6e99caace51864fc5a52ebc7cb3865e4d25c8b7f776223022d0e8"
   license "Unlicense"
 
   depends_on xcode: "13.3"
